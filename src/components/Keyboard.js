9fedc8305d2a29e4ac3de3890a78b8cf86bf1e54
ShaikhAdnan7621 <119574362+ShaikhAdnan7621@users.noreply.github.com>
2024-01-31 23:24:22 +0530

    Add files via upload

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,19 +1,21 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 
 function Key({ letter, activeKey, size }) {
-    if(activeKey === "Control"){
-        activeKey = "Ctr"
-    }
-    if(activeKey === " "){
-        activeKey = "Space"
-    }
+  if (activeKey === "Control") {
+    activeKey = "Ctr";
+  }
+  if (activeKey === " ") {
+    activeKey = "Space";
+  }
   return (
     <button
       style={{ width: `${size * 4}px` }}
       className={` m-1 p-2 border rounded-lg border-gray-500  ${
-        activeKey === letter ? " bg-gray-300 shadow-inner shadow-gray-600 " : " shadow shadow-gray-400 bg-white text-black"
+        activeKey.toUpperCase() === letter
+          ? " bg-gray-300 shadow-inner shadow-gray-600 "
+          : " shadow shadow-gray-400 bg-white text-black"
       }`}
     >
       {letter.toUpperCase()}
@@ -27,53 +29,30 @@ export default function Keyboard({ pressedkey }) {
   useEffect(() => {
     setActiveKey(pressedkey);
   }, [pressedkey]);
-  const numRow = "1234567890-=";
-  const firstRow = "qwertyuiop[]\\";
-  const secondRow = "asdfghjkl;'";
-  const thirdRow = "zxcvbnm,./";
+
+  // prettier-ignore
+  const keyboardLayout = [
+    {"`":8, "1":10, "2":10, "3":10, "4":10, "5":10, "6":10, "7":10, "8":10, "9":10, "0":10, "-":10, "=":10, "Back":21},
+    {"Tab":19, "Q":10, "W":10, "E":10, "R":10, "T":10, "Y":10, "U":10, "I":10, "O":10, "P":10, "[":10, "]":10, "\\":10},
+    {"Caps":19, "A":10, "S":10, "D":10, "F":10, "G":10, "H":10, "J":10, "K":10, "L":10, ";":10, "'":10, "Enter":22},
+    {"SHIFT":26, "Z":10, "X":10, "C":10, "V":10, "B":10, "N":10, "M":10, ",":10, ".":10, "/":10, "shift":27 },
+    {"CTR":12, "Fn":12, "ALT":12, "Space":62, "alt":12, "ctr":12, "<":12, "˄˅":13, ">":12}
+  ];
 
   return (
     <div tabIndex="0" className="border border-gray-500 rounded-lg p-1 ">
-      <div className="flex justify-center">
-        <Key letter={"`"} activeKey={activeKey} key={"`"} size={8} />
-
-        {numRow.split("").map((letter) => (
-          <Key letter={letter} activeKey={activeKey} key={letter} size={10} />
-        ))}
-        <Key letter={"Back"} activeKey={activeKey} key={"Back"} size={21} />
-      </div>
-      <div className="flex justify-center">
-        <Key letter={"Tab"} activeKey={activeKey} key={"Tab"} size={19} />
-
-        {firstRow.split("").map((letter) => (
-          <Key letter={letter} activeKey={activeKey} key={letter} size={10} />
-        ))}
-      </div>
-      <div className="flex justify-center">
-        <Key letter={"Caps"} activeKey={activeKey} key={"C locl"} size={19} />
-        {secondRow.split("").map((letter) => (
-          <Key letter={letter} activeKey={activeKey} key={letter} size={10} />
-        ))}
-        <Key letter={"Enter"} activeKey={activeKey} key={"Enter"} size={22} />
-      </div>
-      <div className="flex justify-center">
-        <Key letter={"Shift"} activeKey={activeKey} key={"Shift"} size={26} />
-        {thirdRow.split("").map((letter) => (
-          <Key letter={letter} activeKey={activeKey} key={letter} size={10} />
-        ))}
-        <Key letter={"Shift"} activeKey={activeKey} key={"Shift2"} size={27} />
-      </div>
-      <div className="flex justify-center">
-        <Key letter={"Ctr"} activeKey={activeKey} key={"Ctr"} size={12} />
-        <Key letter={"Fn"} activeKey={activeKey} key={"Fn"} size={12} />
-        <Key letter={"Alt"} activeKey={activeKey} key={"Alt"} size={12} />
-        <Key letter={"Space"} activeKey={activeKey} key={"Space"} size={62} />
-        <Key letter={"Alt"} activeKey={activeKey} key={"Alt2"} size={12} />
-        <Key letter={"Ctr"} activeKey={activeKey} key={"Ctr2"} size={12} />
-        <Key letter={"<"} activeKey={activeKey} key={"<"} size={12} />
-        <Key letter={"˄˅"} activeKey={activeKey} key={"˄˅"} size={13} />
-        <Key letter={">"} activeKey={activeKey} key={">"} size={12} />
-      </div>
+      {keyboardLayout.map((row, index) => (
+        <div className="flex justify-center" key={index}>
+          {Object.entries(row).map(([letter, size]) => (
+            <Key
+              letter={letter}
+              activeKey={activeKey}
+              key={letter}
+              size={size}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Testbord.js
+++ src/components/Testbord.js
@@ -1,17 +1,33 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Keyboard from "./Keyboard";
-import { space } from "postcss/lib/list";
+import TestResults from "./TestResults";
 
-function Testbord({ para: initialPara }) {
+const Testbord = ({ para: initialPara }) => {
   const [inputText, setInput] = useState("");
-  const [completedWords, setCompletedWords] = useState("");
   const [finalParagraph, setFinalParagraph] = useState([]);
   const [para, setPara] = useState(initialPara);
   const [isTestActive, setIsTestActive] = useState(false);
   const [completed, setCompleted] = useState(false);
   const [countdown, setCountdown] = useState(60);
-  const [activeKey, setActiveKey] = useState(""); // Set countdown start time
+  const [activeKey, setActiveKey] = useState("");
+  const [completedWords, setCompletedWords] = useState("");
+
+  const startTest = () => {
+    setIsTestActive(true);
+    setCountdown(60);
+    setTimeout(() => {
+      setIsTestActive(false);
+      setCompleted(true);
+    }, 60000);
+  };
+
+  const animatekey = (key) => {
+    setActiveKey(key);
+    setTimeout(() => {
+      setActiveKey("");
+    }, 100);
+  };
 
   useEffect(() => {
     if (isTestActive) {
@@ -29,46 +45,17 @@ function Testbord({ para: initialPara }) {
           ]);
           setInput("");
         } else if (
-          ![
-            "Enter",
-            "Shift",
-            "Control",
-            "Alt",
-            "Tab",
-            "CapsLock",
-            "Meta",
-            "Escape",
-            "ArrowRight",
-            "ArrowLeft",
-            "AltGraph",
-            "ArrowUp",
-            "ArrowDown",
-          ].includes(key)
+          // prettier-ignore
+          !["Enter","Shift","Control","Alt","Tab","CapsLock","Meta","Escape","ArrowRight","ArrowLeft","AltGraph","ArrowUp","ArrowDown",].includes(key)
         ) {
           setInput(inputText + key);
         }
       };
       document.addEventListener("keydown", keydownHandler);
-      // Log finalParagraph after 1 minute
       return () => document.removeEventListener("keydown", keydownHandler);
     }
-  }, [inputText, para, completedWords, finalParagraph, isTestActive]);
+  }, [inputText, para, finalParagraph, isTestActive]);
 
-  const startTest = () => {
-    setIsTestActive(true);
-    setCountdown(60);
-    setTimeout(() => {
-      setIsTestActive(false);
-      setCompleted(true);
-    }, 60000);
-  };
-
-  const animatekey = (key) => {
-    setActiveKey(key);
-    setTimeout(() => {
-      setActiveKey("");
-    }, 100);
-  };
   useEffect(() => {
     if (countdown > 0) {
       const timerId = setTimeout(() => setCountdown(countdown - 1), 1000);
@@ -79,12 +66,10 @@ function Testbord({ para: initialPara }) {
   return (
     <div className="items-center justify-between h-screen pt-10">
       <h1 className="h-10 text-xl text-center">
-        {" "}
         {isTestActive && <span>Remaning Time {countdown}</span>}
       </h1>
-
-      <div className="flex justify-start items-center mt-8">
-        <div className="w-1/2">
+      <div className="flex">
+        <div className="w-1/2 h-14">
           <h1 className="text-4xl font-normal text-nowrap font-sans flex justify-end gap-3 w-full overflow-hidden">
             {completedWords.split(" ").map((word, index) => (
               <span key={index} className="opacity-40 h-14">
@@ -99,7 +84,7 @@ function Testbord({ para: initialPara }) {
               <span
                 key={index}
                 className={`h-14 ${
-                  index === 0 ? "opacity-100 font-semibold" : "opacity-40"
+                  index === 0 ? "opacity-100 font-semibold " : "opacity-40"
                 }`}
               >
                 {word}
@@ -108,6 +93,7 @@ function Testbord({ para: initialPara }) {
           </h1>
         </div>
       </div>
+
       <div className="flex justify-start items-center">
         <div className="w-1/2 h-14">
           <h1 className="text-4xl font-normal text-nowrap font-sans overflow-hidden gap-3 flex justify-end">
@@ -137,10 +123,10 @@ function Testbord({ para: initialPara }) {
         <Keyboard pressedkey={activeKey} />
       </div>
       {!isTestActive && !completed && (
-        <div className="absolute top-0 ring-0 left-0 bottom-0 w-screen h-screen flex justify-center items-center bg-gray-500 bg-opacity-50 ">
-          <div className=" rounded-full  p-2 bg-slate-200 shadow-2xl shadow-black  ">
+        <div className="absolute top-0 ring-0 left-0 bottom-0 w-screen h-screen flex justify-center items-center bg-gray-500 bg-opacity-50">
+          <div className="rounded-full p-2 bg-slate-200 shadow-2xl shadow-black">
             <button
-              className="bg-gray-100 hover:bg-white text-black  font-bold active:scale-95 h-52 w-52 rounded-full shadow-inner  shadow-gray-500 focus:scale-105 text-3xl border-2 border-gray-500"
+              className="bg-gray-100 hover:bg-white text-black font-bold active:scale-95 h-52 w-52 rounded-full shadow-inner shadow-gray-500 focus:scale-105 text-3xl border-2 border-gray-500"
               onClick={startTest}
               tabIndex={1}
               autoFocus={true}
@@ -150,110 +136,9 @@ function Testbord({ para: initialPara }) {
           </div>
         </div>
       )}
-      {completed && (
-        <div className="absolute top-0 ring-0 left-0 bottom-0 w-screen h-screen flex justify-center items-center bg-gray-500 bg-opacity-50 ">
-          <div className=" rounded-lg p-2 bg-slate-200 shadow-2xl shadow-black  ">
-            <div className="bg-gray-100 text-black h-56 w-72 sm:w-96 rounded-lg  shadow border-2 border-gray-500 p-3">
-              <h1 className="text-2xl font-semibold text-center my-2">
-                Test Completed
-              </h1>
-
-              <div className=" flex justify-around px-5 items-center">
-                <div>
-                  <div
-                    className={` w-14 h-14 border rounded-full flex justify-center mx-auto items-center  ${
-                      finalParagraph.length < 20
-                        ? "bg-red-500"
-                        : " bg-green-500 "
-                    }`}
-                  >
-                    {finalParagraph.length}
-                  </div>
-                  <p className="text-center">PWM</p>
-                </div>
-                <div>
-                  <div
-                    className={` w-14 h-14 border rounded-full flex justify-center mx-auto items-center ${
-                      (finalParagraph.filter(({ isCorrect }) => isCorrect)
-                        .length /
-                        finalParagraph.length) *
-                        100 >
-                      90
-                        ? " bg-green-500 "
-                        : " bg-red-500 "
-                    } `}
-                  >
-                    {(finalParagraph.filter(({ isCorrect }) => isCorrect)
-                      .length /
-                      finalParagraph.length) *
-                      100}
-                    %
-                  </div>
-                  <p className="text-center">Acuracy</p>
-                </div>
-                <div>
-                  <div
-                    className={`w-14 h-14 border rounded-full flex justify-center mx-auto items-center ${
-                      finalParagraph.filter(({ isCorrect }) => isCorrect)
-                        .length < 20
-                        ? "bg-red-500"
-                        : " bg-green-500 "
-                    } `}
-                  >
-                    {finalParagraph.filter(({ isCorrect }) => isCorrect).length}
-                  </div>
-                  <p className="text-center">Score</p>
-                </div>
-              </div>
-              <div className="mt-5 text-center">
-                <p>
-                  Your Score is{" "}
-                  <span
-                    className={` ${
-                      finalParagraph.filter(({ isCorrect }) => isCorrect)
-                        .length < 20
-                        ? "text-red-500"
-                        : " text-green-500 "
-                    }`}
-                  >
-                    {finalParagraph.filter(({ isCorrect }) => isCorrect).length} WPM
-                  </span>{" "}
-                  out of{" "}
-                  <span
-                    className={` ${
-                      finalParagraph.length < 20
-                        ? "text-red-500"
-                        : " text-green-500 "
-                    }`}
-                  >
-                    {finalParagraph.length} WPM
-                  </span>{" "}
-                  with Acuraccy of{" "}
-                  <span
-                    className={` ${
-                      (finalParagraph.filter(({ isCorrect }) => isCorrect)
-                        .length /
-                        finalParagraph.length) *
-                        100 <
-                      20
-                        ? "text-red-500"
-                        : " text-green-500 "
-                    }`}
-                  >
-                    {(finalParagraph.filter(({ isCorrect }) => isCorrect)
-                      .length /
-                      finalParagraph.length) *
-                      100}
-                    %
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {completed && <TestResults finalParagraph={finalParagraph} />}
     </div>
   );
-}
+};
 
 export default Testbord;
